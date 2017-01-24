c894a47a9c8c95e8f287a7c070f954182fa510de
mblix <m.blix@samsung.com>
2017-01-23 18:26:22 -0800

    Update example

---
+++ Assets/ClickBehavior.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ClickBehavior : MonoBehaviour {
+
+    private Camera cam;
+
+	void Start() {
+        cam = GetComponent<Camera>();
+
+        cam.clearFlags = CameraClearFlags.SolidColor;
+
+        cam.backgroundColor = new Color(0.1f, 0.2f, 0.3f, 1.0f);
+    }
+	
+	void Update() {
+        if (Input.GetMouseButtonUp(0))
+        {
+            cam.backgroundColor = new Color(
+                Random.Range(0f, 0.5f), Random.Range(0f, 0.5f), Random.Range(0f, 0.5f)
+                );
+        }
+    }
+}
--- Assets/CubeCreator.cs
+++ Assets/CubeCreator.cs
@@ -1,11 +1,21 @@
 ﻿using UnityEngine;
 using System.Collections;
 
+/*
+ * Create the Cube Sea
+ TODO:
+ -add background: color change on click
+ -add rotating cubes
+*/
+
 public class CubeCreator : MonoBehaviour {
 
-    public Transform cube;
+    public GameObject cube;
 
-	void Start () {
+    public GameObject rotatingCubes;
+    private readonly float RotationSpeed = 0.5f;
+
+    void Start() {
         int gridSize = 10;
 	    for (int x = 0; x < gridSize; ++x)
         {
@@ -13,15 +23,38 @@ public class CubeCreator : MonoBehaviour {
             {
                 for (int z = 0; z < gridSize; ++z)
                 {
-                    if (x == 0 && y == 0 && z == 0)
+                    Vector3 position = new Vector3(x - gridSize / 2, y - gridSize / 2, z - gridSize / 2);
+                    if (position.x == 0 && position.y == 0 && position.z == 0)
+                    { // TODO: correct, float issue? works, so why cube at 0
+                        Debug.Log("zero");
+                        Debug.Log(position.x);
                         continue;
-                    Instantiate(cube, new Vector3(x - gridSize/2, y - gridSize/2, z - gridSize/2), Quaternion.Euler(0, 180, 0));
+                    }
+                    //Instantiate(cube, position, Quaternion.Euler(0, 180, 0));
                 }
             }
         }
-	}
 
-	void Update () {
+        // TODO: merge
+        // https://docs.unity3d.com/ScriptReference/Mesh.CombineMeshes.html
 
-	}
+        // TODO: make rotating cubes
+        // = [ [0, 0.25, -0.8], [0.8, 0.25, 0], [0, 0.25, 0.8], [-0.8, 0.25, 0] ];
+        rotatingCubes.transform.position.Set(0f, 0.25f, 0f);
+        GameObject _cube;
+        _cube = Instantiate(cube, rotatingCubes.transform) as GameObject;
+        _cube.transform.position.Set(0f, 0f, -0.8f);
+        _cube = Instantiate(cube, rotatingCubes.transform) as GameObject;
+        _cube.transform.position.Set(0f, 0f, 0.8f);
+        _cube = Instantiate(cube, rotatingCubes.transform) as GameObject;
+        _cube.transform.position.Set(0.8f, 0f, 0f);
+        _cube = Instantiate(cube, rotatingCubes.transform) as GameObject;
+        _cube.transform.position.Set(-0.8f, 0f, 0f);
+    }
+
+    void Update() {
+        // TODO: rotating cubes
+        rotatingCubes.transform.Rotate(Vector3.up * (RotationSpeed * Time.deltaTime));
+
+    }
 }
